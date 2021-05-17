2180c25d4c2752c956b34588a7c1f8420cbf2f10
Matthias De Smet <11850640+matthdsm@users.noreply.github.com>
2021-05-17 15:33:44 +0200

    update module order, disable interop module

--- multiqc_sav/multiqc_sav.py
+++ multiqc_sav/multiqc_sav.py
@@ -17,6 +17,15 @@ def update_config():
     log.info("SAV: Updating search patterns")
     # Add module to module order
     config.module_order.append({"SAV": {"module_tag": ["DNA", "RNA", "BCL", "Demultiplex"]}})
+
+    # Move module to the top
+    config.top_modules.append("SAV")
+
+    # Disable InterOp module to avoid duplicate data
+    disabled_modules = ["interop"]
+    for module in disabled_modules:
+        del config.avail_modules[module]
+
     # Update search patterns
     if "SAV/xml" not in config.sp:
         config.update_dict(config.sp, {"SAV/xml": {"fn_re": ".*(RunInfo|RunParameters)\.xml"}})
